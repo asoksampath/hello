c38aca42677a7899f645adaed1bceca701ddff4f
asoksampath <424004@cognizant.com>
2020-01-27 12:57:26 +0400

    added new message to ChildComponent.Js

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -1,7 +1,7 @@
 import { LightningElement,api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
-    @api valueFromParent = 'Child Value';
+    @api valueFromParent = 'Child Value new commit';
 
     @api showValueFromParent(showValue){
         // eslint-disable-next-line no-alert
